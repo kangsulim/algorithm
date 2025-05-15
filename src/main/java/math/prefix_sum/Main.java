0083ffa59e6b16c59485a4befff3eb00c87ef0a8
kangsulim <rkdtnfla1@naver.com>
2025-05-15 17:55:13 +0900

    123

--- src/main/java/math/prefix_sum/Main.java
+++ src/main/java/math/prefix_sum/Main.java
@@ -1,21 +1,18 @@
 package math.prefix_sum;
 
 import java.util.Scanner;
-
+// https://www.acmicpc.net/problem/11659
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int N = scan.nextInt();
-        scan.nextLine();
         int M = scan.nextInt();
-        scan.nextLine();
 
         long[] A = new long[M + 1];
 
         for (int k = 1; k < N + 1; k++) {
             A[k] = scan.nextInt();
-            scan.nextLine();
         }
 
         long[] S = new long[M + 1];
@@ -31,9 +28,7 @@ public class Main {
 
         for (int k = 1; k < M + 1; k++) {
             int i = scan.nextInt();
-            scan.nextLine();
             int j = scan.nextInt();
-            scan.nextLine();
 
             System.out.println(S[j] - S[i - 1]);
         }
